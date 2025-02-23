8f552c518eaa418b47ed96719b9582910fed6949
HumbertDominique <dominique@cadej.org>
2025-02-23 13:59:57 +0100

    folder strucutre

---
+++ ows/ows.py
@@ -0,0 +1,12 @@
+def function_name(parameters):
+    """
+    Brief description of the function.
+
+    Parameters:
+    parameters (type): Description of the parameters.
+
+    Returns:
+    return_type: Description of the return value.
+    """
+    # TODO: Implement the function logic here
+    return
